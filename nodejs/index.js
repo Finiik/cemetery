f9f116f05cf8975323548b8f550c756271b3ef9d
Oleh Finiak <finako243@gmail.com>
2023-05-03 00:55:47 +0300

    smth special

---
+++ nodejs/index.js
@@ -0,0 +1,22 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('mydb.sqlite');
+
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++){
+    stmt.run("Kovalchuk" + i);
+  }
+  stmt.finalize();
+
+  db.serialize(function() {
+    db.run("INSERT INTO users (name) VALUES ('John')");
+  });
+
+  db.each("Select rowid AS id, info FROM lorem", function(err, row){
+    console.log(row.id+ ": " + row.info);
+  });
+});
+
+
+db.close();
