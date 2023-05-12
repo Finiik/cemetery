afad276a73a238447daf6afdee6980dc2c10b9e5
Oleh Finiak <finako243@gmail.com>
2023-05-12 10:50:42 +0300

    fourth page and db

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,23 +1,6 @@
-/*const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('mydb.sqlite');
+const sqlite3 = require('sqlite3').verbose();
 
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++){
-    stmt.run("Kovalchuk" + i);
-  }
-  stmt.finalize();
-
-  db.serialize(function() {
-    db.run("INSERT INTO users (name) VALUES ('John')");
-  });
-
-  db.each("Select rowid AS id, info FROM lorem", function(err, row){
-    console.log(row.id+ ": " + row.info);
-  });
-});
-
-
-db.close();
-*/
+const db = new sqlite3.Database('./mydb.sqbpro', sqlite3.OPEN_READWRITE, (err)=>{
+    if (err) return console.error(err.message)
+    else console.log("All right")
+})
